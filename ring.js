8da408eceaabef36f44beff4ea46f42777917359
EVOKE <evoke.lektrique@gmail.com>
2021-10-18 23:25:17 +0330

    Update UI, Add event listeners to buttons

--- ring.js
+++ ring.js
@@ -6,23 +6,27 @@
 const Ring = {
 
    /**
-    * Total servers
+    * Total nodes
     *
     * @type {Array}
     */
-   servers: [],
+   nodes: [],
 
    /**
-    * Add a server to ring servers list
+    * Add a node to ring nodes list
     *
     * @param {String}  name Host name
     * @param {String}  host Host ip address
     */
-   add_server(name, host) {
-      this.servers.push({
+   add_node(name, host) {
+      const node = {
          name: name,
          host: host
-      });
+      };
+
+      this.nodes.push(node);
+
+      return node;
    },
 
    /**
@@ -67,21 +71,77 @@ const Ring = {
 
 };
 
-(async () => {
+// (async () => {
 
-   const ring = Ring;
+//    const ring = Ring;
 
-   ring.add_server('A', '0.0.0.0');
-   ring.add_server('B', '0.0.0.1');
-   ring.add_server('C', '0.0.0.2');
+//    ring.add_node('A', '0.0.0.0');
+//    ring.add_node('B', '0.0.0.1');
+//    ring.add_node('C', '0.0.0.2');
 
-   const total_servers = ring.servers.length;
+//    const total_nodes = ring.nodes.length;
 
-   const message = "Test";
-   const hash    = await Ring.hash(message);
-   const index   = ring.convert_to_integer(hash) % total_servers;
+//    const message = "Test";
+//    const hash    = await Ring.hash(message);
+//    const index   = ring.convert_to_integer(hash) % total_nodes;
 
-   const destination = ring.servers[index];
-   console.log(destination);
+//    const destination = ring.nodes[index];
+//    console.log(destination);
 
-})();
+// })();
+
+
+function get(id) {
+   return document.getElementById(id);
+}
+
+function generate_random_ip() {
+   let ip = "";
+   let i = 0;
+   while(i < 4) {
+      ip += Math.floor(Math.random() * 255);
+      if(i > 2) {
+         break;
+      } else {
+         ip += "."
+      }
+      i += 1
+   }
+
+   return ip;
+}
+
+function generate_random_name() {
+   const names = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+   const index = names[Math.floor(Math.random() * names.length)];
+
+   return index;
+}
+
+const ring = Ring;
+
+get('add_node').addEventListener('click', e => {
+   e.preventDefault();
+   const node = ring.add_node(generate_random_name(), generate_random_ip());
+   console.log(ring)
+   add_node(node);
+});
+
+
+function add_node(node) {
+   const wrapper   = document.createElement('div');
+   const node_name = document.createElement('span');
+   const node_host = document.createElement('span');
+
+   wrapper.classList.add('node');
+   node_name.classList.add('name');
+   node_host.classList.add('host');
+
+   node_name.innerText = node.name;
+   node_host.innerText = node.host;
+
+   wrapper.appendChild(node_name);
+   wrapper.appendChild(node_host);
+
+   get('flatten_ring').appendChild(wrapper);
+}
